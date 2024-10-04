cc46141fb9c4e6d4c96994803355858c613bf9d8
updatemike <115986306+updatemike@users.noreply.github.com>
2024-10-04 13:34:44 +0100

    Fix case sensitivity in ordering Lists

--- mike/proj/hva/core/sorter/SortById.java
+++ mike/proj/hva/core/sorter/SortById.java
@@ -6,6 +6,6 @@ import hva.core.Identifiable;
 public class SortById<T extends Identifiable> implements Comparator<T>{
     @Override
     public int compare(T t1, T t2) {
-        return t1.getId().compareTo(t2.getId());
+        return (t1.getId().toLowerCase()).compareTo(t2.getId().toLowerCase());
     }
 }
