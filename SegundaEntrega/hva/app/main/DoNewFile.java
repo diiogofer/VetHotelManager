03078bf38fd57eac9a6dcd8ceabeb7083cb8fb55
updatemike <115986306+updatemike@users.noreply.github.com>
2024-10-10 00:25:34 +0100

    Merge try catch block

--- SegundaEntrega/hva/app/main/DoNewFile.java
+++ SegundaEntrega/hva/app/main/DoNewFile.java
@@ -1,8 +1,6 @@
 package hva.app.main;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import hva.app.exception.FileOpenFailedException;
 import hva.core.Hotel;
 import hva.core.HotelManager;
@@ -10,7 +8,6 @@ import hva.core.exception.MissingFileAssociationException;
 import pt.tecnico.uilib.forms.Form;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Command for creating a new zoo hotel.
@@ -21,27 +18,26 @@ class DoNewFile extends Command<HotelManager> {
   }
 
   @Override
-  protected final void execute() throws CommandException {
+protected final void execute() throws CommandException {
     Hotel hotel = _receiver.getHotel();
-    if(hotel == null || hotel.getHotelState() == false || Form.confirm(Prompt.saveBeforeExit()) == false){
-      _receiver.createHotel();
-      return ;
+    
+    // If no hotel exists or hotel hasn't changed or saving is not confirmed, create a new hotel and return.
+    if (hotel == null || !hotel.getHotelState() || !Form.confirm(Prompt.saveBeforeExit())) {
+        _receiver.createHotel();
+        return;
     }
-    if(_receiver.getFileName() == null){
-      try{
-        String filename = Form.requestString(Prompt.newSaveAs());
-        _receiver.saveAs(filename);
-      } catch (MissingFileAssociationException | IOException ex){
+    try {
+        // Save with or without an existing filename
+        if (_receiver.getFileName() == null) {
+            String filename = Form.requestString(Prompt.newSaveAs());
+            _receiver.saveAs(filename);
+        } else {
+            _receiver.save();
+        }
+    } catch (MissingFileAssociationException | IOException ex) {
         throw new FileOpenFailedException(ex);
-      }
     }
-    else {
-      try{
-        _receiver.save();
-      } catch (MissingFileAssociationException | IOException ex) {
-        throw new FileOpenFailedException(ex);
-      }
-    }
-    _receiver.createHotel();   
-  }
+
+    _receiver.createHotel();  // Create a new hotel after saving
+}
 }
