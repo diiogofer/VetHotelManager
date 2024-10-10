5e6b5cc08357abbed90a499006cd0c10eb3bafe7
Diogo Fernandes <diiogofer@icloud.com>
2024-10-10 22:30:06 +0100

    Employee javadoc

--- SegundaEntrega/hva/core/Employee.java
+++ SegundaEntrega/hva/core/Employee.java
@@ -1,17 +1,57 @@
 package hva.core;
 
+/**
+ * Represents an abstract Employee class, which defines the basic attributes and behaviors 
+ * of an employee, including an identifier, name, and methods to calculate satisfaction, 
+ * get responsibilities, and represent the type of employee.
+ *
+ * @see Identified
+ */
 public abstract class Employee extends Identified{
+    /** The name of the employee. */
     private String _name;
 
+    /**
+     * Constructs an Employee object with the specified identifier and name.
+     * 
+     * @param identifier the unique identifier for the employee
+     * @param name the name of the employee
+     */
     Employee(String identifier, String name) {
         super(identifier);
         _name = name;
     }
 
+    /**
+     * Calculates the satisfaction level of the employee. 
+     * Concrete subclasses must implement this method.
+     * 
+     * @return an integer representing the satisfaction level
+     */
     abstract int calculateSatisfaction();
+
+    /**
+     * Returns the type of employee as a string.
+     * Concrete subclasses must implement this method to describe the specific type of employee.
+     * 
+     * @return a string representing the type of employee
+     */
     abstract String getEmployeeTypeToString();
+
+    /**
+     * Returns the responsibilities of the employee as a string.
+     * Concrete subclasses must implement this method to describe the specific responsibilities of the employee.
+     * 
+     * @return a string representing the employee's responsibilities
+     */
     abstract String getResponsibilitiesToString();
 
+    /**
+     * Returns a string representation of the employee, including the employee type, 
+     * identifier, name, and responsibilities.
+     * 
+     * @return a string representing the employee
+     */
     public String toString() {
         String resposibilityString = getResponsibilitiesToString();
         if (resposibilityString.length() == 0) {
--- SegundaEntrega/hva/core/EmployeeType.java
+++ SegundaEntrega/hva/core/EmployeeType.java
@@ -1,5 +1,17 @@
 package hva.core;
 
+/**
+ * Enum representing the types of employees.
+ * There are two types of employees:
+ * <ul>
+ *   <li>VETERINARIAN - A veterinarian responsible for the health and medical care of the animals.</li>
+ *   <li>KEEPER - A keeper responsible for the maintenance of the habitats.</li>
+ * </ul> 
+ */
 public enum EmployeeType {
-    VETERINARIAN, KEEPER;
+    /** Represents a veterinarian employee. */
+    VETERINARIAN, 
+    
+    /** Represents a keeper employee. */    
+    KEEPER;
 }
