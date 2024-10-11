1c249b9ff4f9220f6b8661a0e8163b8c2fd22e30
Diogo Fernandes <diiogofer@icloud.com>
2024-10-11 11:48:24 +0100

    tree and vaccines javadocs

--- SegundaEntrega/hva/core/TreeType.java
+++ SegundaEntrega/hva/core/TreeType.java
@@ -1,5 +1,19 @@
 package hva.core;
 
+/**
+ * Enum representing the types of trees that can exist.
+ * 
+ * <p>The available tree types are:</p>
+ * <ul>
+ *   <li>PERENE - evergreen tree.</li>
+ *   <li>CADUCA - deciduous tree.</li>
+ * </ul>
+ */
 public enum TreeType {
-    PERENE, CADUCA;
+
+    /** Represents a evergreen tree. */
+    PERENE, 
+    
+    /** Represents a deciduous tree. */
+    CADUCA;
 }
