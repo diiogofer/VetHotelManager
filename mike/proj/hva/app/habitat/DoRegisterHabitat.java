5f30b525ed9933965dbf1ebe40621f1e8c063cd9
updatemike <115986306+updatemike@users.noreply.github.com>
2024-10-12 11:32:40 +0100

    Finish DoRegisterHabitat (4.4.2)

--- mike/proj/hva/app/habitat/DoRegisterHabitat.java
+++ mike/proj/hva/app/habitat/DoRegisterHabitat.java
@@ -4,7 +4,6 @@ import hva.core.Hotel;
 import hva.app.exception.DuplicateHabitatKeyException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Add a new habitat to this zoo hotel.
