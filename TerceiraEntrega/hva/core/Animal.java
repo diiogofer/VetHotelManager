58e8624a7ec1835bc32bf53f3024926c37cb4a0a
Diogo Fernandes <diiogofer@icloud.com>
2024-10-24 18:48:23 +0100

    JavaDocs Animal

--- TerceiraEntrega/hva/core/Animal.java
+++ TerceiraEntrega/hva/core/Animal.java
@@ -4,29 +4,69 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents an animal in the zoo hotel.
+ */
 public class Animal extends Identified {
+  
+  /** The species to which the animal belongs. */
   Species _species;
+
+  /** The habitat where the animal resides. */
   Habitat _habitat;
+
+  /** List of vaccine events associated with the animal. */
   private List<VaccineEvent> _vaccineEventList = new ArrayList<>();
 
-  Animal(String id, String name, Species species, Habitat habitat) {
-    super(id, name);
+
+  /**
+   * Constructs an Animal with the given identifier, name, species, and habitat.
+   * 
+   * @param animalId The unique identifier of the animal.
+   * @param name The name of the animal.
+   * @param species The species of the animal.
+   * @param habitat The habitat where the animal resides.
+   */
+  Animal(String animalId, String name, Species species, Habitat habitat) {
+    super(animalId, name);
     _species = species;
     _habitat = habitat;
   }
+
+  /**
+   * Changes the animal's habitat.
+   * 
+   * @param habitat The new habitat for the animal.
+   * @return true if the habitat was changed, false if the new habitat is the same as the current one.
+   */
   boolean changeHabitat(Habitat habitat) {
     if(_habitat != null) {
-      if(_habitat.equals(habitat)) return false;
+      if(_habitat.equals(habitat)) { 
+        return false; 
+      }
       _habitat.removeAnimal(this);
     }
     _habitat = habitat;
     _habitat.addAnimal(this);
     return true;
   }
+
+  /**
+   * Returns a string representation of the animal in the format:
+   * "ANIMAL|id|name|speciesId|vaccineEvents|habitatId".
+   * 
+   * @return A string representation of the animal.
+   */
   public String toString() {
     return "ANIMAL|" + getId() + "|" + getName() + "|" + _species.getId() + "|" + 
       vaccineEventsToString() + "|" + _habitat.getId();
   }
+
+  /**
+   * Converts the vaccine events to a string representation.
+   * 
+   * @return A string of vaccine events or "VOID" if no events exist.
+   */
   private String vaccineEventsToString() {
     if(_vaccineEventList.size() == 0) return "VOID";
 
@@ -35,10 +75,20 @@ public class Animal extends Identified {
     for (VaccineEvent event : _vaccineEventList) {
         damageString.append(event.toCategory()).append(",");
     }
-    // Remove , at end
+    // Remove the last comma
     damageString.setLength(damageString.length() - 1);
     return damageString.toString();
   }
+
+  /**
+   * Calculates the satisfaction level of the animal.
+   * The formula used is:
+   * <pre>
+   * satisfaction = 20 + 3 * sameSpecies - 2 * differentSpecies + area / population + adequacy
+   * </pre>
+   * 
+   * @return The calculated satisfaction level of the animal.
+   */
   int calculateSatisfaction() {
     int same = _habitat.countSpecies(_species) - 1;
     int population = _habitat.countPopulation();
@@ -48,12 +98,29 @@ public class Animal extends Identified {
     return 20 + 3 * same - 2 * different + area / population + adequacy;
   }
 
-  Species getSpecies() {return _species;}
+  /**
+   * Returns the species of the animal.
+   * 
+   * @return The species of the animal.
+   */
+  Species getSpecies() {
+    return _species;
+  }
 
+  /**
+   * Adds a new vaccine event to the animal's vaccine history.
+   * 
+   * @param event The vaccine event to be added.
+   */
   void addVaccineEvent(VaccineEvent event) {
     _vaccineEventList.add(event);
   }
 
+  /**
+   * Returns an unmodifiable list of all vaccine events associated with the animal.
+   * 
+   * @return A list of vaccine events.
+   */
   List<VaccineEvent> getAllVaccineEvents() {
     return Collections.unmodifiableList(_vaccineEventList);
   }
