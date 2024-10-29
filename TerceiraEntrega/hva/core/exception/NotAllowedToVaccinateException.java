be5e7fee6a2a4979b0f75767d75b22a6838d085b
Diogo Fernandes <diiogofer@icloud.com>
2024-10-29 17:30:04 +0000

    Fix wrong vaccination error message

--- TerceiraEntrega/hva/core/exception/NotAllowedToVaccinateException.java
+++ TerceiraEntrega/hva/core/exception/NotAllowedToVaccinateException.java
@@ -2,6 +2,6 @@ package hva.core.exception;
 
 public class NotAllowedToVaccinateException extends Exception{
   public NotAllowedToVaccinateException (String id) {
-    super("Vet not allowed to vaccinate species: " + id);
+    super(id);
   }
 }
